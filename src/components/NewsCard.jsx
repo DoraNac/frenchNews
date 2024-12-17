3be5203313fdea2ae4df9431a3f837f51680beed
Dora <dora.nacinovic99@gmail.com>
2024-12-17 20:34:42 +0100

    Cleaned up index.css unnecessary styles

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,9 +1,8 @@
 const NewsCard = ({ title, description }) => {
   return (
-    <div className="card w-full bg-base-100 shadow-lg hover:shadow-xl transition-all">
+    <div className="card w-full bg-white shadow-lg hover:shadow-xl transition-all">
       <figure>
         <img
-        //dummy photo
           src="https://plus.unsplash.com/premium_photo-1707080369554-359143c6aa0b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D"
           alt="News"
           className="h-48 w-full object-cover"
